2aa02b492f612845b6e6aea3d95a6dc91c6a9790
rbdkqk <rbdkqk@gmail.com>
2020-07-19 03:07:48 +0900

    Updates

---
+++ precache-manifest.dbcc16d741a422a49ddd8150bf286bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b92368e29c6428e7e8df4b9d35c85b0",
+    "url": "/rbdkqk/simple-todo-app/index.html"
+  },
+  {
+    "revision": "e0270f49ad1e55d7e1d6",
+    "url": "/rbdkqk/simple-todo-app/static/css/main.56df6992.chunk.css"
+  },
+  {
+    "revision": "517448722c259d425d02",
+    "url": "/rbdkqk/simple-todo-app/static/js/2.bd25b82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rbdkqk/simple-todo-app/static/js/2.bd25b82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0270f49ad1e55d7e1d6",
+    "url": "/rbdkqk/simple-todo-app/static/js/main.b35f1ffc.chunk.js"
+  },
+  {
+    "revision": "76e11f7ded8729edb956",
+    "url": "/rbdkqk/simple-todo-app/static/js/runtime-main.923880d5.js"
+  }
+]);
