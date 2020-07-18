f7602f1cd1b8edf67f1593f8a38594a171cbf80c
rbdkqk <rbdkqk@gmail.com>
2020-07-19 03:15:59 +0900

    Updates

---
+++ precache-manifest.af049a3be8756c850f2e5ab115e54347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9218bd09e85d64ae31b3a817800ddfde",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "e0270f49ad1e55d7e1d6",
+    "url": "/simple-todo-app/static/css/main.56df6992.chunk.css"
+  },
+  {
+    "revision": "517448722c259d425d02",
+    "url": "/simple-todo-app/static/js/2.bd25b82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.bd25b82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0270f49ad1e55d7e1d6",
+    "url": "/simple-todo-app/static/js/main.b35f1ffc.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rbdkqk/simple-todo-app/precache-manifest.dbcc16d741a422a49ddd8150bf286bdc.js"
+  "/simple-todo-app/precache-manifest.af049a3be8756c850f2e5ab115e54347.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rbdkqk/simple-todo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
